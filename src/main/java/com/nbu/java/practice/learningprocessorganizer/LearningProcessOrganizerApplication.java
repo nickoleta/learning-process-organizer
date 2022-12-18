aa8a516330f521a54b4f5415c52cfca5117cfcb5
yankovan <yankovan@vmware.com>
2022-12-18 12:11:19 +0200

    Initializing the repository

---
+++ src/main/java/com/nbu/java/practice/learningprocessorganizer/LearningProcessOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.nbu.java.practice.learningprocessorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningProcessOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningProcessOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nbu/java/practice/learningprocessorganizer/LearningProcessOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbu.java.practice.learningprocessorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningProcessOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
